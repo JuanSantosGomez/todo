506d325957c251a1dd6b660a81cb456bc8e7b144
popotses <juansantosgomez@protonmail.com>
2022-10-30 21:17:17 +0800

    Complete commit

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
 
-    path('', views.index)
+    path('', views.index, name="list"),
+    path('update_task/<str:pk>/',views.updateTask,name="update_task"),
+    path('delete_task/<str:pk>/',views.deleteTask,name="delete_task")
 
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
